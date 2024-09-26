d26aa296b86ede603ca610ff8d4ab4ef0ca9dd65
233 <ayusummer233@vip.qq.com>
2024-09-26 18:48:10 +0800

    ✨ Java class to Base64 and revert

---
+++ Java/CommonUse/Encode/Base64/ClassToBase64/ClassToBase64.java
@@ -0,0 +1,25 @@
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Base64;
+
+public class ClassToBase64 {
+    public static void main(String[] args) {
+        try {
+            // 读取.class文件
+            File file = new File("resource/BasicFilter.class");
+            FileInputStream fis = new FileInputStream(file);
+            byte[] bytes = new byte[(int) file.length()];
+            fis.read(bytes);
+            fis.close();
+
+            // 将字节数组进行Base64编码
+            String encoded = Base64.getEncoder().encodeToString(bytes);
+
+            // 输出Base64编码后的字符串
+            System.out.println(encoded);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
