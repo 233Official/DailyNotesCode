b1b53bc8e75774afa9daa40e3fd4e2378acae0dd
233 <ayusummer233@vip.qq.com>
2024-10-21 16:24:08 +0800

    ✨ Spring Controller 内存马

--- Java/CommonUse/Encode/Base64/ClassToBase64/ClassToBase64.java
+++ Java/CommonUse/Encode/Base64/ClassToBase64/ClassToBase64.java
@@ -7,7 +7,7 @@ public class ClassToBase64 {
     public static void main(String[] args) {
         try {
             // 读取.class文件
-            File file = new File("resource/SummerCMDListener.class");
+            File file = new File("resource/SummerControllerCMD.class");
             FileInputStream fis = new FileInputStream(file);
             byte[] bytes = new byte[(int) file.length()];
             fis.read(bytes);
